5b563844b28e2a96f51e966ed64d8e29256d99a1
Gunda Abhishek <gunda.abhishek@phonepe.com>
2024-01-14 20:03:41 +0530

    storage account list

--- modules/services/storage_account/main.tf
+++ modules/services/storage_account/main.tf
@@ -11,9 +11,13 @@
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 */
+resource "random_id" "saname" {
+  byte_length = 5
+  prefix = var.name
+}
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = var.name
+  name                     = random_id.saname.hex
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = var.account_tier
