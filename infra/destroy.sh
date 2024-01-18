555e6b23a1e3ddbda1ec5d46dd6032dd2cbbe946
Gunda Abhishek <gunda.abhishek@phonepe.com>
2024-01-18 15:54:00 +0530

    documentation for values.yaml and destroying terraform backend

--- infra/destroy.sh
+++ infra/destroy.sh
@@ -17,7 +17,10 @@ export ARM_CLIENT_SECRET="$(yq eval '.client_tenant.client_secret' values.yaml)"
 export ARM_SUBSCRIPTION_ID="$(yq eval '.client_tenant.subscription_id' values.yaml)"
 export ARM_TENANT_ID="$(yq eval '.client_tenant.tenant_id' values.yaml)"
 
-echo "Step 1: Destroy Infrastructure"
-az login --service-principal --username $(yq eval '.client_tenant.client_id' values.yaml) --password $(yq eval '.client_tenant.client_secret' values.yaml) --tenant $(yq eval '.client_tenant.tenant_id' values.yaml)
 
-terraform destroy 
+az login --service-principal --username $(yq eval '.client_tenant.client_id' values.yaml) --password $(yq eval '.client_tenant.client_secret' values.yaml) --tenant $(yq eval '.client_tenant.tenant_id' values.yaml)
+echo "Step 1: Destroy Infrastructure"
+terraform destroy
+
+echo "Step 2: Destroying terraform backend resource group"
+az group delete -n terraform
