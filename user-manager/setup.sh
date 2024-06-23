4b1836eb6256d46520179b0d15fbbe1435520429
Gunda Abhishek <gunda.abhishek@phonepe.com>
2024-06-23 14:02:56 +0530

    referencing values from relative paths

--- user-manager/setup.sh
+++ user-manager/setup.sh
@@ -1,18 +1,27 @@
+#!/usr/bin/env bash
+
+# script directory
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Get the values from the relative path
+ROOT_VALUES="$SCRIPT_DIR/../values.yaml"
+LOCAL_VALUES="$SCRIPT_DIR/values.yaml"
+
 echo "Step 2: Setup Client Credentials as ENVIRONMENT"
-export ARM_CLIENT_ID="$(yq eval '.client_tenant.client_id' ../values.yaml)"
-export ARM_CLIENT_SECRET="$(yq eval '.client_tenant.client_secret' ../values.yaml)"
-export ARM_SUBSCRIPTION_ID="$(yq eval '.client_tenant.subscription_id' ../values.yaml)"
-export ARM_TENANT_ID="$(yq eval '.client_tenant.tenant_id' ../values.yaml)"
+export ARM_CLIENT_ID="$(yq eval '.client_tenant.client_id' "$ROOT_VALUES")"
+export ARM_CLIENT_SECRET="$(yq eval '.client_tenant.client_secret' "$ROOT_VALUES")"
+export ARM_SUBSCRIPTION_ID="$(yq eval '.client_tenant.subscription_id' "$ROOT_VALUES")"
+export ARM_TENANT_ID="$(yq eval '.client_tenant.tenant_id' "$ROOT_VALUES")"
 
 echo "Step 3: Create Terraform Backend"
-clientname=$(yq eval '.client_tenant.client_name' ../values.yaml)
+clientname=$(yq eval '.client_tenant.client_name' "$ROOT_VALUES")
 export TENANT="phonepe"
 export TF_RESOURCE_GROUP="terraform"
 export TF_LOCATION="centralindia"
 export TF_STORAGE_ACCOUNT="tfstatephpstage"$clientname
 export TF_STORAGE_CONTAINER="statecontainer"
 
-az login --service-principal --username $(yq eval '.client_tenant.client_id' ../values.yaml) --password $(yq eval '.client_tenant.client_secret' ../values.yaml) --tenant $(yq eval '.client_tenant.tenant_id' ../values.yaml)
+az login --service-principal --username $(yq eval '.client_tenant.client_id' "$ROOT_VALUES") --password $(yq eval '.client_tenant.client_secret' "$ROOT_VALUES") --tenant $(yq eval '.client_tenant.tenant_id' "$ROOT_VALUES")
 
 echo "Step 4: Terraform init"
 terraform init -backend-config="resource_group_name=${TF_RESOURCE_GROUP}" -backend-config="storage_account_name=${TF_STORAGE_ACCOUNT}" -backend-config="container_name=${TF_STORAGE_CONTAINER}" -backend-config="key=usermanagers5.tfstate"
@@ -25,9 +34,9 @@ terraform apply --auto-approve userplan
 
 terraform output -json > terraform_output.json
 
-first_party_users=$(yq eval '.first_party_details.users[].nick_name' values.yaml)
-partner_users=$(yq eval '.partner_details.users[].nick_name' values.yaml)
-keyvault=$(yq eval '.key_vault_name' values.yaml | sed 's/"//g')
+first_party_users=$(yq eval '.first_party_details.users[].nick_name' "$LOCAL_VALUES")
+partner_users=$(yq eval '.partner_details.users[].nick_name' "$LOCAL_VALUES")
+keyvault=$(yq eval '.key_vault_name' "$LOCAL_VALUES" | sed 's/"//g')
 echo "First Party Azure Entra Users"
 for name in $first_party_users;
 do
